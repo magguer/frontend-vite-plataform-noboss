ab39af7575fe682f91d633090118afcd03be5219
magguer <martintelaguer@gmail.com>
2023-05-03 15:44:32 -0300

    user-type-auth-require&no

--- src/hooks/NoAuthRequire.tsx
+++ src/hooks/NoAuthRequire.tsx
@@ -1,9 +1,9 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
-import UserTypes from "../types/UserTypes";
+import { UserType } from "../types/UserTypes";
 
 function NoAuthRequire() {
-    const user = useSelector((state: UserTypes) => state.user);
+    const user = useSelector((state: UserType) => state.user);
 
     if (user) {
         return <Navigate to="/resumen" />;
